9d04f7fb02c433e7159381b55bb5984d9e08b5f5
mahout-boi <kumara5000br@gmail.com>
2026-07-14 21:12:14 -0300

    aaaa

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 5173,
     hmr: {
       overlay: false,
     },
